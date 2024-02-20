1431f605b866afe77401d8f35d19418a0750aadc
Gary1572 <160667661+Gary1572@users.noreply.github.com>
2024-02-20 14:37:44 -0600

    Add files via upload
    1.Install dependencies
    
    sudo apt-get install -y libudev-dev
    sudo apt install cmake xorg-dev libglu1-mesa-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
    sudo apt-get install build-essential
    sudo apt-get install libx11-dev
    sudo apt-get install libxtst-dev
    2.Build glfw
    
    git clone https://github.com/glfw/glfw.git
    cd glfw
    mkdir build
    cd build
    cmake ..
    make
    sudo make install
    3. Clone repo
    
    git clone https://github.com/Nexilist/xap-client.git
    cd xap-client
    4. Build and Run
    
    mkdir build
    cd build
    cmake ..
    make
    sudo ./xapclient
    5. Press Insert to toggle the Menu (You can only interact with the Menu and the game when the menu is active).

---
+++ Offsets.hpp
@@ -0,0 +1,65 @@
+#pragma once
+// GameVersion=v3.0.54.31
+// Updated 1/19/2024
+
+// Core
+constexpr long OFF_REGION = 0x140000000;                      //[Static]->Region
+constexpr long OFF_LEVEL = 0x16da6b0;                         //[Miscellaneous]->LevelName
+constexpr long OFF_LOCAL_PLAYER = 0x2165e48;                  //[Miscellaneous]->LocalPlayer
+constexpr long OFF_ENTITY_LIST = 0x1db73e8;                   //[Miscellaneous]->cl_entitylist
+    
+// Buttons
+constexpr long OFF_IN_ATTACK = 0x073e0bd8;                    //[Buttons]->in_attack
+constexpr long OFF_IN_JUMP = 0x073e0cf0;                      //[Buttons]->in_jump
+constexpr long OFF_IN_DUCK = 0x073e0dd8;                      //[Buttons]->in_duck
+constexpr long OFFSET_IN_FORWARD = 0x073e0b28;                //[Buttons]->in_forward
+constexpr long OFFSET_TRAVERSAL_START_TIME = 0x2ad0;          //[Buttons]->m_traversalStartTime
+constexpr long OFFSET_TRAVERSAL_PROGRESS = 0x2acc;            //[Buttons]->m_traversalProgress
+constexpr long OFFSET_WALL_RUN_START_TIME = 0x355c;           //[Buttons]->m_wallRunStartTime
+constexpr long OFFSET_WALL_RUN_CLEAR_TIME = 0x3560;           //[Buttons]->m_wallRunClearTime
+    
+// Player
+constexpr long OFF_VIEW_MATRIX = 0x11a350;                    //[RecvTable.DT_Player]->ViewMatrix
+constexpr long OFF_VIEW_RENDER = 0x73df958;                   //[RecvTable.DT_Player]->ViewRender
+constexpr long OFF_ZOOMING = 0x1bb1;                          //[RecvTable.DT_Player]->m_bZooming
+constexpr long OFF_LOCAL_ORIGIN = 0x017c;                     //[DataMap.CBaseViewModel]->m_vecAbsOrigin
+constexpr long OFF_ABSVELOCITY = 0x0170;                      //[DataMap.C_BaseEntity]->m_vecAbsVelocity
+constexpr long OFF_TEAM_NUMBER = 0x0328;                      //[RecvTable.DT_BaseEntity]->m_iTeamNum
+constexpr long OFF_CURRENT_HEALTH = 0x02fc;                   //[RecvTable.DT_BaseEntity]->m_iHealth
+constexpr long OFF_CURRENT_SHIELDS = 0x01a0;                  //[RecvTable.DT_BaseEntity]->m_shieldHealth
+constexpr long OFF_NAME = 0x0471;                             //[RecvTable.DT_BaseEntity]->m_iName
+constexpr long OFF_SIGNIFIER_NAME = 0x0468;                   //[RecvTable.DT_BaseEntity]->m_iSignifierName
+constexpr long OFF_LIFE_STATE = 0x0658;                       //[RecvTable.DT_Player]->m_lifeState
+constexpr long OFF_BLEEDOUT_STATE = 0x26c0;                   //[RecvTable.DT_Player]->m_bleedoutState
+constexpr long OFF_LAST_VISIBLE_TIME = 0x196d + 0x3;          //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3
+constexpr long OFF_LAST_AIMEDAT_TIME = 0x196d + 0x3 + 0x8;    //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3 + 0x8
+constexpr long OFF_VIEW_ANGLES = 0x2514 - 0x14;               //[DataMap.C_Player]-> m_ammoPoolCapacity - 0x14
+constexpr long OFF_PUNCH_ANGLES = 0x2418;                     //[?]->m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
+constexpr long OFFSET_BREATH_ANGLES = (OFF_VIEW_ANGLES - 0x10);
+constexpr long OFF_STUDIOHDR = 0xfd0;                         //[Miscellaneous]->CBaseAnimating!m_pStudioHdr
+constexpr long OFF_BONES = 0x0d80 + 0x48;                     //m_nForceBone + 0x48
+constexpr long OFF_CAMERAORIGIN = 0x1eb0;                     //[Miscellaneous]->CPlayer!camera_origin
+constexpr long OFF_MODELNAME = 0x0030;                        //m_ModelName     
+constexpr long OFF_NAMELIST = 0xc5dacc0;                      //[Miscellaneous]->NameList
+constexpr long OFF_YAW = 0x221c - 0x8;                        //m_currentFramePlayer.m_ammoPoolCount - 0x8
+constexpr long OFF_NAMEINDEX = 0x38;                          //nameIndex
+
+// Weapon
+constexpr long OFF_WEAPON_HANDLE = 0x18f4;                    //[RecvTable.DT_Player]->m_latestPrimaryWeapons
+constexpr long OFF_WEAPON_INDEX = 0x1738;                     //[RecvTable.DT_WeaponX]->m_weaponNameIndex
+constexpr long OFF_WEAPON_AMMO = 0x1514;                          // m_ammoInClip
+constexpr long OFF_GRENADE_HANDLE = 0x1904;                   //[RecvTable.DT_Player]->m_latestNonOffhandWeapons
+constexpr long OFF_SKIN = 0x0d18;                             // m_nSkin
+constexpr long OFF_WEAPON_DISCARDED = 0x1559;                 //[RecvTable.DT_WeaponX]->m_discarded
+constexpr long OFFSET_TIME_BASE = 0x2048;                     //m_currentFramePlayer.timeBase
+
+constexpr long OFF_PROJECTILESCALE = 0x04ec;                  //projectile_gravity_scale + BASE o
+constexpr long OFF_PROJECTILESPEED = 0x04e4;                  //projectile_launch_speed + BASE o
+
+// Glow
+constexpr long HIGHLIGHT_TYPE_SIZE = 0x34;
+constexpr long OFF_GLOW_ENABLE = 0x26c;                       //[DT_HighlightSettings].?
+constexpr long OFF_GLOW_THROUGH_WALL = 0x28c;                 //[DT_HighlightSettings].?
+constexpr long OFF_GLOW_FIX = 0x268;
+constexpr long OFF_GLOW_HIGHLIGHT_ID = 0x28C;                 //[DT_HighlightSettings].m_highlightServerActiveStates
+constexpr long OFF_GLOW_HIGHLIGHTS = 0xB943CB0;
---
+++ README.md
@@ -0,0 +1,63 @@
+<details>
+<summary>Debian based dependencies</summary>
+
+## Intall build-essential / libx11-dev / libxtst-dev / libxrender-dev (Mandatory. You only need to do this once) !!!
+`
+sudo apt-get install build-essential libx11-dev libxtst-dev libxrender-dev
+`
+</details>
+
+<details>
+<summary>Arch based dependencies</summary>
+
+## Intall base-devel / libx11 / libxtst / libxrender (Mandatory. You only need to do this once) !!!
+`
+sudo pacman -Sy base-devel libx11 libxtst libxrender
+`
+</details>
+
+### 0. Introduction:
+```
+DISCLAIMER: This project is for educational purposes only.
+Features available are:
+- Aim Assist
+- ESP, including Mini Map and Level Map
+- RCS
+- Trigger Bot
+- Super Glide
+- Skin Changer
+
+CREDITS:
+- arturzxc, Azreol, savageroar07, NeedItem, SchuhuEule, Acknowledge, flixearth and all contributors from the UC forum.
+```
+
+### 1. Download:
+```
+git clone https://github.com/GitDev28/grinder-CG.git
+```
+
+### 2. Get inside the folder
+```
+cd grinder-CG
+```
+
+### 3. Compile the program (The below is a single word command that you need to type into terminal and hit Enter):
+```
+make
+```
+
+### 4. Open Apex
+```
+Open steam and start apex and wait for it to fully start
+```
+
+### 5. Run the cheato
+```
+sudo ./a.out
+```
+
+### 6. Profit
+```
+Unlimited powaaaaaaaaaa!
+```
+![alt text](https://cdn.vox-cdn.com/thumbor/PYVJRRXPhua4a3I2X3n49AIgPZw=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19542877/star_wars6_movie_screencaps.com_13433.jpg)
